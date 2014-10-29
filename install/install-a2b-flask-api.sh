9f23f7cd459fbd1d5ef94ed25fda76725ebe221b
areski <areski@gmail.com>
2014-10-29 16:47:43 +0100

    update install script instructions

--- install/install-a2b-flask-api.sh
+++ install/install-a2b-flask-api.sh
@@ -12,7 +12,7 @@
 
 #
 # To download and run the script on your server :
-# cd /usr/src/ ; wget --no-check-certificate https://raw.github.com/areski/a2billing-flask-api/master/install/install.sh -O install.sh ; bash install.sh
+# cd /usr/src/ ; rm install-a2b-flask-api.sh; rm bash-common-functions.sh; wget --no-check-certificate https://raw.github.com/areski/a2billing-flask-api/master/install/install-a2b-flask-api.sh -O install-a2b-flask-api.sh ; bash install-a2b-flask-api.sh
 #
 
 INSTALL_MODE='CLONE'
