fb8e5955ce1c42569d178fcbe417791d835307a4
areski <areski@gmail.com>
2015-11-26 17:01:06 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# coding=utf-8
+
 #
 # A2Billing-Flask-API License
 # http://www.star2billing.com
@@ -12,43 +15,138 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
+
+import os
 from setuptools import setup, find_packages
-from setuptools.dist import Distribution
-import pkg_resources
-import sys
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
 
 VERSION = '1.0'
 
-install_flag = False
 
-if sys.argv[1] == "install":
-    install_flag = True
+def read(*parts):
+    return open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+
+
+# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+# Note: you may want to copy this into your setup.py file verbatim, as
+# you can't import this from another package, when you don't know if
+# that package is installed yet.
+def find_package_data(where='.', package='',
+                      exclude=standard_exclude,
+                      exclude_directories=standard_exclude_directories,
+                      only_in_packages=True,
+                      show_ignored=False):
+    """
+    Return a dictionary suitable for use in ``package_data``
+    in a distutils ``setup.py`` file.
+
+    The dictionary looks like::
+
+        {'package': [files]}
+
+    Where ``files`` is a list of all the files in that package that
+    don't match anything in ``exclude``.
+
+    If ``only_in_packages`` is true, then top-level directories that
+    are not packages won't be included (but directories under packages
+    will).
+
+    Directories matching any pattern in ``exclude_directories`` will
+    be ignored; by default directories with leading ``.``, ``CVS``,
+    and ``_darcs`` will be ignored.
+
+    If ``show_ignored`` is true, then all the files that aren't
+    included in package data are shown on stderr (for debugging
+    purposes).
+
+    Note patterns use wildcards, or can be exact paths (including
+    leading ``./``), and all searching is case-insensitive.
+    """
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print ("Directory %s ignored by pattern %s" % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print ("File %s ignored by pattern %s" % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix + name)
+    return out
+
 
 setup(
     name='a2billing-flask-api',
     version=VERSION.replace(' ', '-'),
-    description='HTTP API for Khomp SMS',
-    long_description=open('README.rst').read(),
+    description="Restful APIs framework for A2Billing",
+    long_description=read('README.rst'),
+    keywords='a2billing, flask, api, rest',
     author='Belaid Arezqui',
     author_email='areski@gmail.com',
-    url='http://www.cdr-stats.org/',
-    download_url='https://github.com/areski/a2billing-flask-api',
+    url='http://github.com/areski/a2billing-flask-api',
+    license='MPL 2.0 license',
+    # py_modules=['a2billing_flask_api'],
+    namespace_packages=[],
+    # test_suite='tests',
     packages=find_packages(),
-    include_package_data=True,
-    license='MPL 2.0 License',
-    classifiers=[
-        'Development Status :: 1 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Developers, Users',
-        'License :: OSI Approved :: MPL 2.0 License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python, Javascript, HTML',
-        'Topic :: Call Analytic Software'
-    ],
+    package_data=find_package_data(),
     zip_safe=False,
-    setup_requires=[
-        "Flask == 0.8",
-        "gevent == 0.13.7",
+    install_requires=[
+        'setuptools',
+        'Jinja2>=2.8',
+        'Flask==0.8'
+        # -*- Extra requirements: -*-
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
